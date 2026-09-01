418b143ba72b06cf24c0b5e6f3718f5f9f0552a0
CONEXCO <conexco2026@gmail.com>
2026-09-01 13:34:22 -0400

    deploy: v2.5 release final

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@
 
 export const config = {
   // Enlace de descarga del APK de CONEXCO TIENDA
-  storeDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.4.2/app-debug.apk",
+  storeDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.5/app-release.apk",
 
   // Enlace de descarga del APK de CONEXCO CLIENTE
   clientDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.2-cliente/app-release.apk",
