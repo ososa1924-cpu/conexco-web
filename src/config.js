6febf47f9d60b6d75b6ef7aa80740a9db792c6bf
CONEXCO <conexco2026@gmail.com>
2026-09-04 19:28:28 -0400

    v2.8: BD cifrada + instalador fix

--- src/config.js
+++ src/config.js
@@ -7,13 +7,13 @@
 
 export const config = {
   // Enlace de descarga del APK de CONEXCO TIENDA
-  storeDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.7/conexco-tienda-v2.7.apk",
+  storeDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.8/conexco-tienda-v2.8.apk",
 
   // Enlace de descarga de CONEXCO TIENDA para Windows (PC)
-  storeWindowsUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.7/conexco-pc-tienda-v2.6-setup.zip",
+  storeWindowsUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.8/conexco-pc-v2.8-setup.zip",
 
   // Enlace de descarga del APK de CONEXCO CLIENTE
-  clientDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.7/conexco-cliente-v2.7.apk",
+  clientDownloadUrl: "https://github.com/ososa1924-cpu/conexco-apk/releases/download/v2.8/conexco-cliente-v2.8.apk",
 
   // Numero de contacto (se muestra en la seccion de Soporte)
   supportPhone: "0424-237.7576",
